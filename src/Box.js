45dea4219c53cc899be8835d7f23693c67e376f9
Khanh <tran_khanh@outlook.com>
2020-04-28 16:03:41 +0700

    imp- implement box by SWCFT

---
+++ src/Box.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+import {
+  space,
+  width,
+  color,
+  fontSize,
+  textAlign,
+  propTypes,
+} from 'styled-system'
+import theme from './theme'
+
+const Box = styled.div([], space, width, color, fontSize, textAlign)
+
+Box.displayName = 'Box'
+Box.header = Box.withComponent('header')
+Box.main = Box.withComponent('main')
+Box.article = Box.withComponent('article')
+Box.section = Box.withComponent('section')
+Box.footer = Box.withComponent('footer')
+
+Box.defaultProps = {
+  theme,
+}
+
+Box.propTypes = {
+  ...propTypes.textAlign,
+  ...propTypes.fontSize,
+  ...propTypes.space,
+  ...propTypes.color,
+}
+
+export default Box
