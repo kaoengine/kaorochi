c5b75206444b3489c4df6d3c7cf9efb0ec930ebb
Khanh <tran_khanh@outlook.com>
2020-04-28 16:00:40 +0700

    impl-avatar component

---
+++ .storybook/Avatar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { Avatar } from '../src'
+
+storiesOf('Avatar', module)
+  .add(
+    'Avatar component',
+    withInfo({
+      inline: true,
+      text: 'A circular avatar image primitive.',
+    })(() => <Avatar src="https://kaoengine.com/team/zach.jpg" size={64} />)
+  )
+  .add('Team', () => (
+    <>
+      {['zach', 'max', 'lachlan', 'mingjie', 'athul'].map((key) => (
+        <Avatar
+          mr={2}
+          src={`https://kaoengine.com/team/${key}.jpg`}
+          key={key}
+        />
+      ))}
+    </>
+  ))
