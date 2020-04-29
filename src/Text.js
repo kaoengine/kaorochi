1e599096ed53a3fa1bdd4f3fa4c1bcb2e4601b1c
Khanh <tran_khanh@outlook.com>
2020-04-29 08:56:05 +0700

    imp - Text

---
+++ src/Text.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import {
+  fontSize,
+  fontWeight,
+  textAlign,
+  space,
+  color,
+  propTypes,
+} from 'styled-system'
+import theme from './theme'
+
+// Todo: How many type of Typo Attribute?
+export const caps = (props) => props.caps && { textTransform: 'uppercase' }
+
+export const regular = (props) => props.regular && { fontWeight: 'regular' }
+
+export const bold = (props) => props.bold && { fontWeight: 'bold' }
+
+// Todo: Why I use REM not EM @Dieu
+export const maxWidth = ({ maxWidth }) =>
+  maxWidth && { maxWidth: `${maxWidth}rem` }
+
+const Text = styled.p(
+  [],
+  fontSize,
+  space,
+  color,
+  caps,
+  textAlign,
+  bold,
+  regular,
+  fontWeight,
+  maxWidth
+)
+
+Text.displayName = 'Text'
+
+Text.propTypes = {
+  caps: PropTypes.bool,
+  regular: PropTypes.bool,
+  bold: PropTypes.bool,
+  maxWidth: PropTypes.number,
+  ...propTypes.fontSize,
+  ...propTypes.fontWeight,
+  ...propTypes.textAlign,
+  ...propTypes.space,
+  ...propTypes.color,
+}
+
+Text.defaultProps = {
+  theme,
+  m: 0,
+}
+
+Text.span = Text.withComponent('span')
+Text.p = Text.withComponent('p')
+Text.s = Text.withComponent('s')
+
+export default Text
