449b74b0bcb819d1c26c96effe33462f3380732b
Khanh <tran_khanh@outlook.com>
2020-04-29 09:05:46 +0700

    impsb - icon,button,iconbutton

---
+++ .storybook/IconButton.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react'
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { IconButton } from '../src'
+
+storiesOf('IconButton', module)
+  .add(
+    'IconButton component',
+    withInfo({
+      inline: true,
+      text: 'Use the <IconButton /> component to render a primitive button.',
+    })(() => <IconButton glyph="view-close" color="error" />)
+  )
+  .add('Rectangular button', () => (
+    <IconButton glyph="send" bg="info" color="white" />
+  ))
+  .add('Circular button', () => (
+    <IconButton glyph="checkmark" bg="success" color="white" circle />
+  ))
+  .add('Disabled circular buttons', () => (
+    <Fragment>
+      <IconButton disabled glyph="view-close" color="error" />
+      <IconButton disabled glyph="view-close" bg="error" color="white" circle />
+    </Fragment>
+  ))
