70f55335a5f379eddb3f759dacc754bb4e9f356f
Khanh <tran_khanh@outlook.com>
2020-04-29 09:06:04 +0700

    implement LagreButton

---
+++ src/LargeButton.js
@@ -0,0 +1,37 @@
+import Button from './Button'
+import theme, { hexa } from './theme'
+
+const LargeButton = Button.extend`
+  text-transform: uppercase;
+  box-shadow: 0 2px 12px ${({ theme }) => theme.shadowColor};
+
+  &:hover,
+  &:focus {
+    box-shadow: 0 2px 12px 2px
+      ${(props) =>
+        props.inverted ? props.theme.shadowColor : hexa(props.bg, 0.25)};
+  }
+
+  &:active {
+    box-shadow: 0 4px 16px 2px
+      ${(props) =>
+        props.inverted ? props.theme.shadowColor : hexa(props.bg, 0.25)};
+  }
+`
+
+LargeButton.displayName = 'LargeButton'
+
+LargeButton.defaultProps = {
+  theme,
+  bg: 'primary',
+  color: 'white',
+  fontSize: 3,
+  m: 0,
+  py: 3,
+  px: 4,
+}
+
+LargeButton.button = LargeButton.withComponent('button')
+LargeButton.input = LargeButton.withComponent('input')
+
+export default LargeButton
